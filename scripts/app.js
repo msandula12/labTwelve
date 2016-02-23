920ae35208906609e3298cc67a8db080c7a8ae60
Michael Sandula <msandula@gmail.com>
2016-02-23 15:36:35 -0500

    User gets alerted if item field is empty upon click

--- scripts/app.js
+++ scripts/app.js
@@ -3,11 +3,21 @@ var app = angular.module('myModule', []);
 
 app.controller('myController', function(){
 		var main = this;
-		main.list = ['Take a shower', 'Make bed', 'Eat breakfast', 'Wash dishes'];
-		// main.add = "";
+		main.list = [
+			'Take a shower', 
+			'Make bed', 
+			'Eat breakfast', 
+			'Wash dishes', 
+			'Feed the cat', 
+			'Go to grocery store', 
+			'Do laundry'
+		];
 		main.addItemToList = function(input) {
-			main.list.push(main.add);
-			return true;
+			if (!main.add)
+				alert("Please enter a task");
+			else
+				main.list.push(main.add);
+				return true;
 		};
 		main.remove = function(item) { 
   			var index = main.list.indexOf(item);
@@ -16,8 +26,12 @@ app.controller('myController', function(){
 });
 
 
-// $(document).ready(function() {
-// 	$("#btnRemove").hover(function() {
-// 			$(this).css("background-color", "#ff0000").text("remove");
-// 	});
-// });
+$(document).ready(function() {
+	$(".btnRemove").hover(
+		function() {
+			$(this).css({"background-color" : "#ff0000", "cursor" : "pointer"}).text("Remove");
+		}, function() {
+			$(this).css("background-color", "#b20000").text("Task done?");
+		}
+	);
+});
